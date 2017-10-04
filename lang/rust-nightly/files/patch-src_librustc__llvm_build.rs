3c097af52425c56ad458ac5b558a33fef717e0ff
Greg V <greg@unrelenting.technology>
2017-10-04 12:27:05 +0300

    lang/rust-nightly: revive the port by starting over from lang/rust

--- lang/rust-nightly/files/patch-src_librustc__llvm_build.rs
+++ lang/rust-nightly/files/patch-src_librustc__llvm_build.rs
@@ -1,11 +1,11 @@
---- src/librustc_llvm/build.rs.orig	2017-01-18 02:20:03 UTC
+--- src/librustc_llvm/build.rs.orig	2017-06-06 00:42:59 UTC
 +++ src/librustc_llvm/build.rs
-@@ -234,6 +234,8 @@ fn main() {
-     // OpenBSD has a particular C++ runtime library name
+@@ -241,6 +241,8 @@ fn main() {
      let stdcppname = if target.contains("openbsd") {
+         // OpenBSD has a particular C++ runtime library name
          "estdc++"
 +    } else if target.contains("freebsd") {
 +        "c++"
-     } else {
-         "stdc++"
-     };
+     } else if target.contains("netbsd") && llvm_static_stdcpp.is_some() {
+         // NetBSD uses a separate library when relocation is required
+         "stdc++_pic"
