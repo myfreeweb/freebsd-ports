9f0c7e728306332d1fb15ada306456ea8e5e1179
Greg V <greg@unrelenting.technology>
2017-10-04 14:44:16 +0300

    Update mesa patches, attempt 2

--- graphics/mesa-dri/files/patch-src_intel_vulkan_anv__queue.c
+++ graphics/mesa-dri/files/patch-src_intel_vulkan_anv__queue.c
@@ -20,15 +20,3 @@
  
   fail:
     anv_bo_pool_free(&device->batch_bo_pool, &bo);
-Drop header that was never used
-
---- src/intel/vulkan/anv_queue.c.orig	2017-08-12 16:09:52 UTC
-+++ src/intel/vulkan/anv_queue.c
-@@ -27,7 +27,6 @@
- 
- #include <fcntl.h>
- #include <unistd.h>
--#include <sys/eventfd.h>
- 
- #include "anv_private.h"
- #include "vk_util.h"
