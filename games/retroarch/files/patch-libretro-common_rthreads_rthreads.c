e5adcfa484073ad791a8c16bff2c901e915d0247
Greg V <greg@unrelenting.technology>
2018-04-07 17:18:12 +0300

    games/retroarch: 1.7.5

--- games/retroarch/files/patch-libretro-common_rthreads_rthreads.c
+++ games/retroarch/files/patch-libretro-common_rthreads_rthreads.c
@@ -1,6 +1,6 @@
---- libretro-common/rthreads/rthreads.c.orig	2016-07-16 23:05:18 UTC
+--- libretro-common/rthreads/rthreads.c.orig	2018-12-31 20:08:25 UTC
 +++ libretro-common/rthreads/rthreads.c
-@@ -57,6 +57,8 @@
+@@ -69,6 +69,8 @@
  #include <mach/mach.h>
  #endif
  
