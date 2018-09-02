aade6dc979d982bf00963841baba88ee72084b4a
Jan Beich <jbeich@FreeBSD.org>
2018-09-02 05:36:26 +0000

    graphics/vulkan-loader: pet portlint
    $ portlint -AC
    WARN: graphics/vulkan-loader/pkg-plist: There are only 4 items in the plist.  Consider using PLIST_FILES instead of pkg-plist when installing less than 6 items.
    WARN: graphics/vulkan-loader/files/patch-CMakeLists.txt: patch was not generated using ``make makepatch''.  It is recommended to use ``make makepatch'' when you need to [re-]generate a patch to ensure proper patch format.
    WARN: graphics/vulkan-loader/files/patch-loader_CMakeLists.txt: patch was not generated using ``make makepatch''.  It is recommended to use ``make makepatch'' when you need to [re-]generate a patch to ensure proper patch format.
    WARN: graphics/vulkan-loader/files/patch-loader_loader.c: patch was not generated using ``make makepatch''.  It is recommended to use ``make makepatch'' when you need to [re-]generate a patch to ensure proper patch format.
    WARN: graphics/vulkan-loader/files/patch-loader_vk__loader__platform.h: patch was not generated using ``make makepatch''.  It is recommended to use ``make makepatch'' when you need to [re-]generate a patch to ensure proper patch format.
    0 fatal errors and 5 warnings found.

--- graphics/vulkan-loader/files/patch-loader_vk__loader__platform.h
+++ graphics/vulkan-loader/files/patch-loader_vk__loader__platform.h
@@ -1,5 +1,5 @@
---- loader/vk_loader_platform.h.orig	2018-08-07 21:04:13.856591000 +0300
-+++ loader/vk_loader_platform.h	2018-08-07 21:04:14.881279000 +0300
+--- loader/vk_loader_platform.h.orig	2018-08-03 22:10:42 UTC
++++ loader/vk_loader_platform.h
 @@ -31,7 +31,7 @@
  #include "vulkan/vk_platform.h"
  #include "vulkan/vk_sdk_platform.h"
