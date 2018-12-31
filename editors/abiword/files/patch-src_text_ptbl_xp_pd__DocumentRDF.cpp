552e2ed981ac166624730ccc17e119312f9fbe91
Greg V <greg@unrelenting.technology>
2018-12-31 21:47:40 +0300

    editors/abiword: upd

--- editors/abiword/files/patch-src_text_ptbl_xp_pd__DocumentRDF.cpp
+++ editors/abiword/files/patch-src_text_ptbl_xp_pd__DocumentRDF.cpp
@@ -1,26 +1,13 @@
---- src/text/ptbl/xp/pd_DocumentRDF.cpp.orig	2015-03-31 15:14:42 UTC
+--- src/text/ptbl/xp/pd_DocumentRDF.cpp.orig	2018-11-03 20:28:18 UTC
 +++ src/text/ptbl/xp/pd_DocumentRDF.cpp
-@@ -570,6 +570,11 @@ bool PD_URI::operator==(const std::strin
-     return m_value == b;
- }
+@@ -2269,8 +2269,8 @@ PD_RDFEvent::exportToFile( const std::string& filename
+         icalcomponent_set_uid( c,         m_uid.c_str() );
+         icalcomponent_set_location( c,    m_location.c_str() );
+         icalcomponent_set_description( c, m_desc.c_str() );
+-        icalcomponent_set_dtstart( c,     icaltime_from_timet( m_dtstart, 0 ) );
+-        icalcomponent_set_dtend( c,       icaltime_from_timet( m_dtend, 0 ) );
++        icalcomponent_set_dtstart( c,     icaltime_from_timet_with_zone( m_dtstart, 0, NULL ) );
++        icalcomponent_set_dtend( c,       icaltime_from_timet_with_zone( m_dtend, 0, NULL ) );
  
-+bool PD_URI::operator<(const PD_URI& b) const
-+{
-+    return m_value < b.m_value;
-+}
-+
- PD_URI
- PD_URI::prefixedToURI( PD_RDFModelHandle model ) const
- {
-@@ -611,11 +616,6 @@ bool PD_URI::write( std::ostream& ss ) c
-     return true;
- }
- 
--bool operator<( PD_URI a, PD_URI b)
--{
--    return a.toString() < b.toString();
--}
--
- bool operator<( std::pair< PD_URI, PD_URI > a, PD_URI b)
- {
-     return a.first.toString() < b.toString();
+         char* data = icalcomponent_as_ical_string( c );
+         std::ofstream oss( filename.c_str() );
